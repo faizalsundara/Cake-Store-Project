9eb40bede81a5d36b5812b64bca0187d2fb4d1e2
FaizalSundara <faizalramadhansundara@gmail.com>
2022-08-10 03:55:24 +0700

    update file env

--- config/mysqlDriver.go
+++ config/mysqlDriver.go
@@ -9,7 +9,17 @@ import (
 )
 
 func ConnectionDB() *sql.DB {
-	dbConnection := os.Getenv("DB_CONNECTION")
+	dbUsername := os.Getenv("DB_Username")
+	dbPassword := os.Getenv("DB_Password")
+	dbPort := os.Getenv("DB_Port")
+	dbHost := os.Getenv("DB_Host")
+	dbName := os.Getenv("DB_Name")
+	dbConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True&loc=Local",
+		dbUsername,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName)
 	db, err := sql.Open("mysql", dbConnection)
 	if err != nil {
 		panic(err.Error())
